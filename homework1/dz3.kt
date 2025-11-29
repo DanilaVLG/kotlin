313217b4196ae3ee3e4bb202c9c42ef912920923
данила влг <generatorsent2001@gmail.com>
2025-11-29 11:54:02 +0300

    Добавил домашнее задание 3

---
+++ homework1/dz3.kt
@@ -0,0 +1,24 @@
+fun main(){
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+    print("Год полета - ")
+    println(year)
+    print("Взлет - ")
+    print(hour)
+    print(":")
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+    
+    print("Посадка - ")
+    print(hour)
+    print(":")
+    print(minute)
+
+
+
+
+
+}
