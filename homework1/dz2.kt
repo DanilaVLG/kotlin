056645053a5355c722e4c3c5ab19f571b9c0ae87
данила влг <generatorsent2001@gmail.com>
2025-11-29 11:15:29 +0300

    Добавил домашнее задание 2

---
+++ homework1/dz2.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var total_zakaz = 75
+    var text_zakaz = "все норм, пупайте"
+    var count_worker = 2000
+    
+
+
+    println("Кол-во заказов: $total_zakaz")
+    println("Лучший отзыв: $text_zakaz")
+    println("Кол-во работников: $count_worker")
+    count_worker = count_worker-1
+    println("Кол-во работников после оптимизации: $count_worker")
+
+    
+}
